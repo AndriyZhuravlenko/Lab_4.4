f9f257cd55d1d87ffb183ead81b4795070c6e3d6
LEGION <andrii.zhuravlenko.ik.2021@lpnu.ua>
2021-10-06 18:26:10 +0300

    Add project files.

---
+++ Lab_4.4/Source.cpp
@@ -0,0 +1,52 @@
+// Lab_04_4.cpp 
+// Журавленко Андрій 
+// Лабораторна робота № 4.4
+// Табуляція функції, заданої графіком
+// Варіант 6 
+
+#include <iomanip>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double x, y, xp, xk, dx, R;
+
+	cout << "R = "; cin >> R;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= -5)
+			y = -3;
+		else
+			if (-5 < x && x <= -R)
+				y = (3. * R + 3. * x) / (-R + 5);
+			else
+				if (-R < x && x <= R)
+					y = sqrt(pow(R, 2) - pow(x, 2));
+				else
+					if (R < x && x <= 8)
+						y = (R * x - pow(R, 2)) / (8 - R);
+					else
+						y = R;
+
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+
+	return 0;
+}
